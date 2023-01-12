448917ca0ccd086c6b49c0f77e196a75be6a4faf
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2023-01-13 00:23:02 +0900

    used a reducer for making this app

--- creating-shopping-app-redux/src/store.js
+++ creating-shopping-app-redux/src/store.js
@@ -1,5 +1,8 @@
 import {configureStore} from "@reduxjs/toolkit";
+import cartReducer from "./features/shoppingCart/CartSlice";
 
 export const store = configureStore({
-    reducer:{},
+    reducer:{
+        cart: cartReducer,
+    },
 });
