0b4a89f01bb5d1c57be9dc19cf77ab3de63bab41
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2023-01-14 22:25:04 +0900

    made an UI for showing a Items in shopping cart

---
+++ creating-shopping-app-redux/src/components/CartItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const CartItem = ({ id, img, title, price, amount }) => {
+  return (
+    <article className="cart-item">
+      <img src={img} alt="This is a picture of what you want to buy" />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">{price}yen</h4>
+        <button className="remove-btn">remove</button>
+      </div>
+      <div>
+        <button className="amount-btn">➕</button>
+        <p className="amount">{amount}</p>
+        <button className="amount-btn">➖</button>
+      </div>
+    </article>
+  );
+};
+
+export default CartItem;
