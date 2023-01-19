4b762b46e1d210d53694da1d0fa5dbad47ede665
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2023-01-19 23:04:27 +0900

    ⚡️  installing a usedDispatch to use a state in compo

--- creating-shopping-app-redux/src/components/CartItem.js
+++ creating-shopping-app-redux/src/components/CartItem.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { removeItems } from "../features/shoppingCart/CartSlice";
 
 const CartItem = ({ id, img, title, price, amount }) => {
+    const dispatch = useDispatch();
   return (
     <article className="cart-item">
       <img src={img} alt="This is a picture of what you want to buy" />
       <div>
         <h4>{title}</h4>
         <h4 className="item-price">{price}yen</h4>
-        <button className="remove-btn">remove</button>
+        <button className="remove-btn" onClick={()=>dispatch(removeItems(id))}>remove</button>
+        
       </div>
       <div>
         <button className="amount-btn">+</button>
@@ -17,5 +21,6 @@ const CartItem = ({ id, img, title, price, amount }) => {
     </article>
   );
 };
-
+//Consoleに表示されているPayloadはデータのことを指しており、
+//この場合だと親コンポーネント化らデータを読み込んでそれぞれUIに表示しているが、その中のどのデータに削除ボタンが反映されているのかについてわからないため、Idによって識別する。
 export default CartItem;
